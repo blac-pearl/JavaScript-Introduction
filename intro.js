767c17e3085b9c7db02c317b933e9b6a6891eff2
Chituru Precious <chiturupreciouspearl@gmail.com>
2022-06-30 08:35:12 +0100

    Major changes

---
+++ intro.js
@@ -0,0 +1,7 @@
+console.log("Hello")
+
+alert('My Home')
+
+document.write('<h2>My name is Chituru Precious</h2>');
+document.write("<h3>I am 5'8ft Tall</h3>");
+document.write('<p>I am a Nigerian</p>');
